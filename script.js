7a1c819117efa19ea86b000ffcf3142f6262e641
lyly dev <lyanne.n619@gmail.com>
2025-03-27 13:05:36 +1000

    boleen test

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const listeMots=["Cachalot","Pétunia","Serviette"]
+let score = 0
+let motUtilisateur=prompt("Entre le mot : " +listeMots[0])
+if (motUtilisateur === listeMots[0]){
+    score++
+} 
+
+motUtilisateur=prompt("Entre le mot : " +listeMots[1])
+if(motUtilisateur ===listeMots[1]){
+    score++
+}
+
+motUtilisateur=prompt("Entre le mot : "+listeMots[2])
+if(motUtilisateur === listeMots[2]){
+    score++
+}
+
+console.log(score);
