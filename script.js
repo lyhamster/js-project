c00724e99732bd8d107388ce548028582b3e70bb
lyly dev <lyanne.n619@gmail.com>
2025-03-31 17:28:15 +1000

    new indentation

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 const listeMots=["Cachalot","Pétunia","Serviette"];
 const listePhrases=["Pas de panique !","La vie, l'univers et le reste", "Merci pour le poisson"]
-let score = 0
+let score= 0
 
-let choix=prompt("Choisis entre 'mots' ou 'phrases'")
-while ( choix !== "mots" && choix !== "phrases"){
-    choix=prompt("Choisis entre 'mots' ou 'phrases'")
+let choix= prompt("Choisis entre 'mots' ou 'phrases'")
+    while (choix !== "mots" && choix !== "phrases"){
+        choix= prompt("Choisis entre 'mots' ou 'phrases'")
 }
 
 if (choix==="mots"){
-for (let i = 0; i < listeMots.length; i++){
-    let motUtilisateur=prompt("entrez le mot : " +listeMots[i])
-    if (motUtilisateur === listeMots[i]) {
-        score ++
+    for (let i = 0; i < listeMots.length; i++){
+        let motUtilisateur=prompt("entrez le mot : " +listeMots[i])
+    if (motUtilisateur === listeMots[i]){
+        score ++}
     }
-}
 console.log("Votre score est de " +score+ " sur" + listeMots.length)
 }else{
     for (let i=0; i<listePhrases.length;i++){
         let motUtilisateur=prompt("Entrez le mot :" +listePhrases[i])
-        if (motUtilisateur=== listePhrases[i]) {
-            score ++
-        }
+    if (motUtilisateur=== listePhrases[i]){
+        score ++}
     }
-    console.log("votre score est de " + score + " sur " + listePhrases.length)
+console.log("votre score est de " + score + " sur " + listePhrases.length)
 }
 
 
