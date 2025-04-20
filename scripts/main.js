83510f050a9487e4e7e58a0b7fcdd86536fe0cf1
lyly dev <lyanne.n619@gmail.com>
2025-04-20 18:33:27 +1000

    add click event - update counter at each click + display of the array in the div + button disabled when there are no words in the array

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,18 @@
 
 lancerJeu()
-let listInputRadio = document.querySelectorAll(".zoneProposition input")
-for (let i=0; i < listInputRadio.length; i++){
-    console.log(listInputRadio[i]);
-}
+// let listInputRadio = document.querySelectorAll(".zoneProposition input")
+// for (let i=0; i < listInputRadio.length; i++){
+//     console.log(listInputRadio[i]);
+// }
 
-let userInputText = document.getElementById("usertypings")
-console.log(userInputText)
+// let userInputText = document.getElementById("usertypings")
+// console.log(userInputText)
 
-let submitButton = document.getElementById("submitButton")
-console.log(submitButton)
+// // let submitButton = document.getElementById("submitButton")
+// // console.log(submitButton)
 
-let InputGeneratedText = document.querySelector("#generated-text")
-console.log(InputGeneratedText)
+// let InputGeneratedText = document.querySelector("display-generatedtext")
+// console.log(InputGeneratedText)
 
-let displayScore = document.querySelector(".score span")
-console.log(displayScore)
+// let displayScore = document.querySelector(".score span")
+// console.log(displayScore)
--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,67 @@
 function afficherResultat(score, nbMotsProposes){
-    document.querySelector(".score span").innerHTML = `${score}/${nbMotsProposes}`;
+    document.querySelector("score span").innerHTML = `${score}/${nbMotsProposes}`;
 }
 
 
-function choisirPhrasesOuMots(){
-    let choix= prompt("Choisir entre 'mots' ou 'phrases'")
-    while (choix !== "mots" && choix !== "phrases") {
-        choix = prompt("Choisir entre 'mots' ou 'phrases'")
-    }
-    return choix
+// // function choisirPhrasesOuMots(){
+// //     // let choix= prompt("Choisir entre 'mots' ou 'phrases'")
+// //     while (choix !== "mots" && choix !== "phrases") {
+// //         // choix = prompt("Choisir entre 'mots' ou 'phrases'")
+// //     }
+// //     return choix
+// // }
+
+
+// function lancerBoucleDeJeu(listePropositions){
+//     let score=0
+//     for (let i = 0; i < listePropositions.length; i++){
+//         // let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
+//         if (motUtilisateur === listePropositions[i]){
+//             score++
+//         }
+//     }
+//     return score
+// }
+function displayGeneratedWords(proposition){
+    document.querySelector(".display-generatedtext").innerText= proposition
+
 }
-function lancerBoucleDeJeu(listePropositions){
-    let score=0
-    for (let i = 0; i < listePropositions.length; i++){
-        let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
-        if (motUtilisateur === listePropositions[i]) {
-            score++
-        }
-    }
-    return score
-}
+
 function lancerJeu() {
-    let choix= choisirPhrasesOuMots()
     let score=0
     let nbMotsProposes=0
-    
-    if (choix === "mots") {
-        score = lancerBoucleDeJeu(listeMots)
-        nbMotsProposes = listeMots.length
-    } else {
-        score = lancerBoucleDeJeu(listePhrases)
-        nbMotsProposes = listePhrases.length
-    }
+    let i=0
 
+    let inputUserTypings=document.getElementById("usertypings")
+    let validateButton= document.getElementById("submitButton")
+
+    displayGeneratedWords(listeMots[i])
+    validateButton.addEventListener("click",() => {
+        console.log(inputUserTypings.value)
+        i++
+        displayGeneratedWords(listeMots[i])
+        if (i >= listeMots.length){
+            displayGeneratedWords("fin du game pétasse")
+            validateButton.disabled=true
+        }
+     
+           
+            
+    })
+
+
+   
+    
+
+
+    // // if (choix === "mots") {
+    //     score = lancerBoucleDeJeu(listeMots)
+    //     nbMotsProposes = listeMots.length
+    // // } else {
+    // //   //  score = lancerBoucleDeJeu(listePhrases)
+    // //     nbMotsProposes = listePhrases.length
+    // // 
+    
     afficherResultat(score, nbMotsProposes)
 }
+
