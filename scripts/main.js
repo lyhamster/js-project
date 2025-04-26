46c1f7c94b7ef40e8375226a5e3dd11ef11bd0c6
lyly dev <lyanne.n619@gmail.com>
2025-04-26 22:45:05 +1000

    feat function resetGame()

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,1 @@
-
 lancerJeu()
-// let listInputRadio = document.querySelectorAll(".zoneProposition input")
-// for (let i=0; i < listInputRadio.length; i++){
-//     console.log(listInputRadio[i]);
-// }
-
-// let userInputText = document.getElementById("usertypings")
-// console.log(userInputText)
-
-// // let submitButton = document.getElementById("submitButton")
-// // console.log(submitButton)
-
-// let InputGeneratedText = document.querySelector("display-generatedtext")
-// console.log(InputGeneratedText)
-
-// let displayScore = document.querySelector(".score span")
-// console.log(displayScore)
--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,18 @@
+let score=0
+let i=0
+let nbMotsProposes=0
+let propositionList=listeMots
+let gameFinished= false
+
+let inputUserTypings=document.getElementById("usertypings")
+let validateButton= document.getElementById("submitButton")
+
 function afficherResultat(score,nbMotsProposes){
     document.querySelector(".score span").innerHTML = `${score} /${nbMotsProposes}`
 }
 
-
-// // function choisirPhrasesOuMots(){
-// //     // let choix= prompt("Choisir entre 'mots' ou 'phrases'")
-// //     while (choix !== "mots" && choix !== "phrases") {
-// //         // choix = prompt("Choisir entre 'mots' ou 'phrases'")
-// //     }
-// //     return choix
-// // }
-
-
-// function lancerBoucleDeJeu(listePropositions){
-//     let score=0
-//     for (let i = 0; i < listePropositions.length; i++){
-//         // let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
-//         if (motUtilisateur === listePropositions[i]){
-//             score++
-//         }
-//     }
-//     return score
-// }
 function displayGeneratedWords(proposition){
     document.querySelector(".display-generatedtext").innerText= proposition
-
 }
 
 function selectAllRadio(){
@@ -32,32 +20,7 @@ function selectAllRadio(){
 }
 
 function lancerJeu() {
-    let score=0
-    let i=0
-    let nbMotsProposes=0
-    
-    let propositionList=listeMots
-
-    let inputUserTypings=document.getElementById("usertypings")
-    let validateButton= document.getElementById("submitButton")
     displayGeneratedWords("fais ton choix bitch")
-  
-    validateButton.addEventListener("click",() => {
-        if (inputUserTypings.value===propositionList[i]){
-            score++
-            nbMotsProposes=propositionList.length
-        } 
-        i++
-        displayGeneratedWords(propositionList[i])
-        console.log(inputUserTypings.value)
-        
-        inputUserTypings.value = ""
-        if (i >= propositionList.length){
-            displayGeneratedWords("fin du game pétasse")
-            validateButton.disabled=true
-            afficherResultat(score,nbMotsProposes)
-        }
-    })
 
     selectAllRadio().forEach(radio => {
         radio.addEventListener("change",(event) =>{
@@ -72,12 +35,47 @@ function lancerJeu() {
         })
     })
 
-    selectAllRadio().forEach(radio => {
-        radio.addEventListener("click",()=>{
-            
-        })
+    validateButton.addEventListener("click",() => {
+        if (inputUserTypings.value===propositionList[i]){
+            score++
+            nbMotsProposes=propositionList.length
+        } 
+        i++
+        displayGeneratedWords(propositionList[i])
+        console.log(inputUserTypings.value)
+        
+        inputUserTypings.value = ""
+        if (i >= propositionList.length){
+            displayGeneratedWords("fin du game pétasse")
+            validateButton.disabled=true
+            gameFinished=true
+            afficherResultat(score,nbMotsProposes)
+        }
     })
 
-
+    selectAllRadio().forEach(radio=>{
+        radio.addEventListener("click",(event)=>{
+            if (gameFinished){
+                resetGame()
+            }
+            if(event.target.value==="mots"){
+                propositionList = listeMots
+            }else{
+                propositionList=listePhrases
+            }
+            displayGeneratedWords(propositionList[i])
+        })
+    })
 }
   
+function resetGame () {
+    score = 0;
+    i = 0;
+    nbMotsProposes = 0;
+    inputUserTypings.value = "";
+    validateButton.disabled = false;
+    gameFinished = false; 
+   afficherResultat(0,0)
+}
+
+
