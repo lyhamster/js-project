2efdf44795072002a8296c7588385ab77edd84a9
lyly dev <lyanne.n619@gmail.com>
2025-05-04 00:39:26 +1000

    html/css adjusted input sizes, added partager button and created pop up display div // JS added popupDisplay() function to show the pop up div

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,2 @@
-lancerJeu()
+lancerJeu();
+popupDisplay();
--- scripts/script.js
+++ scripts/script.js
@@ -1,81 +1,94 @@
-let score=0
-let i=0
-let nbMotsProposes=0
-let propositionList=listeMots
-let gameFinished= false
+"use strict";
+let score=0;
+let i=0;
+let nbMotsProposes=0;
+let propositionList=listeMots;
+let gameFinished= false;
 
-let inputUserTypings=document.getElementById("usertypings")
-let validateButton= document.getElementById("submitButton")
+
+
+let inputUserTypings=document.getElementById("usertypings");
+let validateButton= document.getElementById("submitButton");
 
 function afficherResultat(score,nbMotsProposes){
-    document.querySelector(".score span").innerHTML = `${score} /${nbMotsProposes}`
+    document.querySelector(".score span").innerHTML = `${score} /${nbMotsProposes}`;
 }
 
 function displayGeneratedWords(proposition){
-    document.querySelector(".display-generatedtext").innerText= proposition
+    document.querySelector(".display-generatedtext").innerText= proposition;
 }
 
 function selectAllRadio(){
-    return document.querySelectorAll(".zoneProposition input")
+    return document.querySelectorAll(".zoneProposition input");
 }
 
-function lancerJeu() {
-    displayGeneratedWords("fais ton choix bitch")
 
+function lancerJeu() {
+   
     selectAllRadio().forEach(radio => {
         radio.addEventListener("change",(event) =>{
-            console.log(event.target.value)
+            console.log(event.target.value);
             if(event.target.value === "mots"){
-                propositionList=listeMots
+                propositionList=listeMots;
             }
             else{
-                propositionList=listePhrases
+                propositionList=listePhrases;
             }
-            displayGeneratedWords(propositionList[i])
-        })
-    })
+            displayGeneratedWords(propositionList[i]);
+        });
+    });
 
+    displayGeneratedWords("fais ton choix bitch");
     validateButton.addEventListener("click",() => {
         if (inputUserTypings.value===propositionList[i]){
-            score++
-            nbMotsProposes=propositionList.length
-        } 
-        i++
-        displayGeneratedWords(propositionList[i])
-        console.log(inputUserTypings.value)
-        
-        inputUserTypings.value = ""
-        if (i >= propositionList.length){
-            displayGeneratedWords("fin du game pétasse")
-            validateButton.disabled=true
-            gameFinished=true
-            afficherResultat(score,nbMotsProposes)
+            score++;
+            nbMotsProposes=propositionList.length;
         }
-    })
+        i++;
+        displayGeneratedWords(propositionList[i]);
+        console.log(inputUserTypings.value);
+        
+        inputUserTypings.value = "";
+        if (i >= propositionList.length){
+            displayGeneratedWords("fin du game pétasse");
+            validateButton.disabled=true;
+            gameFinished=true;
+            afficherResultat(score,nbMotsProposes);
+        }
+    });
 
     selectAllRadio().forEach(radio=>{
         radio.addEventListener("click",(event)=>{
             if (gameFinished){
-                resetGame()
+                resetGame();
             }
             if(event.target.value==="mots"){
-                propositionList = listeMots
+                propositionList = listeMots;
             }else{
-                propositionList=listePhrases
+                propositionList=listePhrases;
             }
-            displayGeneratedWords(propositionList[i])
+            displayGeneratedWords(propositionList[i]);
         })
-    })
+    });
 }
   
 function resetGame () {
     score = 0;
     i = 0;
     nbMotsProposes = 0;
-    inputUserTypings.value = "";
     validateButton.disabled = false;
     gameFinished = false; 
-   afficherResultat(0,0)
+   afficherResultat(0,0);
 }
 
+function popupDisplay() {
+    let shareResultBtn= document.getElementById("sharedButton");
+    let overlayForm = document.querySelectorAll(".overlay-popup");
+    let closePopup = document.querySelector("body");
 
+    shareResultBtn.addEventListener("click",(event)=>{ 
+        overlayForm.forEach((form)=>{
+        form.style.display="flex";
+        })
+    })
+}
