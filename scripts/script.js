8a046f4308eb5bb5e15effec81bf1e04b7d06ee2
lyly dev <lyanne.n619@gmail.com>
2025-05-22 00:15:15 +1000

    clean up indentation and spacing, implement try-catch in function manageForm(), for better handling

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,7 @@
  * @param {*} score : user's score
  * @param {*} nbMotsProposes : number of words showed to the user to type
  */
-
-function afficherResultat(score,nbMotsProposes){
+function afficherResultat(score, nbMotsProposes) {
     document.querySelector(".score span").innerHTML = `${score} /${nbMotsProposes}`;
 }
 
@@ -16,20 +15,22 @@ function afficherResultat(score,nbMotsProposes){
  * In the zone "display-generatedtext"
  * @param {*} proposition : the words or sentenced displayed
  */
-
-function displayGeneratedWords(proposition){
-    document.querySelector(".display-generatedtext").innerText= proposition;
+function displayGeneratedWords(proposition) {
+    document.querySelector(".display-generatedtext").innerText = proposition;
 }
 
 /**
  * This function build and display the email
- * @param {*} yourName  : user name
- * @param {*} yourEmail : email of the person with whom the user want to share the score.
- * @param {*} score : the score 
+ * @param {string} yourName  : user name
+ * @param {string} yourEmail : email of the person with whom the user want to share the score.
+ * @param {number} score
  */
-function openMailBox (yourName, yourEmail, score) {
-    let mailto=`mailto:${yourEmail}?subject= Partage du score Azertype que Lyly a codé elle même &body= Yo bg, c'est ${yourName} et je viens de réaliser le score de ${score}, c kdo`;    
-    window.location.href= mailto;
+function openMailBox(yourName, yourEmail, score) {
+    const subject = `Partage du score Azertype que Lyly a codé elle même`;
+    const body = `Yo bg, c'est ${yourName} et je viens de réaliser le score de ${score}, c kdo`;
+
+    let mailto=`mailto:${yourEmail}?subject=${subject}&body=${body}`;    
+    window.location.href = mailto;
 }
 
 /**
@@ -37,98 +38,111 @@ function openMailBox (yourName, yourEmail, score) {
  * @param {*} firstName : user name
  * @returns 
  */
-function verifyName(firstName){
+function verifyName(firstName) {
     if (firstName.value.length <= 2)
-    throw new Error("The name should at least have 2 letters")
-   
+        throw new Error("The name should at least have 2 letters");
 }
 
 /**
  * This function check if the email is valid
- * @param {*} mailValidity 
+ * @param {string} mail 
  */
-function verifyEmail(mailValidity){
-    let isEmailValid = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
-   
-    if (!isEmailValid.test(mailValidity)){ 
-    throw new Error("Error email is not valid")
-    }
+function verifyEmail(mail) {
+    const isEmailValid = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z]{2,}$", "i");
+
+    if (!isEmailValid.test(mail))
+        throw new Error ("The email is not valid");
 }
 
 /**
  * This function enables the user to pick between words or sentences before starting typings game
  * @returns select both radios buttons
  */
-function selectAllRadio(){
+function selectAllRadio() {
     return document.querySelectorAll(".zoneProposition input");
 }
 
 
 function lancerJeu() {
     initAddEventListenerPopup();
-    let i=0;
+    let i = 0;
     let score = 0;
-    let nbMotsProposes=0;
-    let propositionList=listeMots;
-    let gameFinished= false;
-
+    let nbMotsProposes = 0;
+    let propositionList = listeMots;
+    let gameFinished = false;
    
     selectAllRadio().forEach(radio => {
-        radio.addEventListener("change",(event) =>{
+        radio.addEventListener("change", (event) => {
             console.log(event.target.value);
-            if(event.target.value === "mots"){
-                propositionList=listeMots;
+            if (event.target.value === "mots") {
+                propositionList = listeMots;
+            } else {
+                propositionList = listePhrases;
             }
-            else{
-                propositionList=listePhrases;
-            }
+        
             displayGeneratedWords(propositionList[i]);
         });
     });
 
     displayGeneratedWords("fais ton choix bitch");
-    let inputUserTypings=document.getElementById("usertypings");
-    let validateButton= document.getElementById("submitButton");
-    validateButton.addEventListener("click",() => {
-        if (inputUserTypings.value===propositionList[i]){
+    const inputUserTypings = document.getElementById("usertypings");
+    const validateButton = document.getElementById("submitButton");
+    validateButton.addEventListener("click", () => {
+        if (inputUserTypings.value === propositionList[i]) {
             score++;
             nbMotsProposes=propositionList.length;
         }
+
         i++;
         displayGeneratedWords(propositionList[i]);
         console.log(inputUserTypings.value);
-        
         inputUserTypings.value = "";
-        if (i >= propositionList.length){
+
+        if (i >= propositionList.length) {
             displayGeneratedWords("fin du game pétasse");
-            validateButton.disabled=true;
-            gameFinished=true;
-            afficherResultat(score,nbMotsProposes);
+            validateButton.disabled = true;
+            gameFinished = true;
+            afficherResultat(score, nbMotsProposes);
         }
+
+        const gameResultToShare = `${score}/${nbMotsProposes}`
+        manageForm(gameResultToShare);
     });
 
-    selectAllRadio().forEach(radio=>{
-        radio.addEventListener("click",(event)=>{
+
+    selectAllRadio().forEach((radio) => {
+        radio.addEventListener("click", () => {
             if (gameFinished){
-                resetGame();}
+                resetGame();
+            }
         });
     });
-
-    let sentForm = document.getElementById("shareForm")
-    sentForm.addEventListener("submit",(event)=>{
-        event.preventDefault();
-        let nameInput = document.getElementById("user-name")
-        let emailInput = document.getElementById("email")
-
-        let emailValue= document.getElementById("email")
-        let nameValue = document.getElementById("user-name")
-        
-        // if (verifyName(nameInput) && verifyEmail(emailInput)){
-            openMailBox(nameValue,emailValue,`${score}/${i}`);
-        // }
-    })
 }
   
-function resetGame () {
+function resetGame() {
    window.location.reload()
 }
+
+function manageForm(shareScore) {
+    const sentForm = document.getElementById("shareForm");
+    sentForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        
+        const nameInput = document.getElementById("user-name");
+        const nameValue = nameInput.value;
+        const emailValue = document.getElementById("email");
+
+       try {
+        verifyName(nameInput);
+        verifyEmail(emailValue.value);
+        openMailBox(nameValue,emailValue,shareScore);
+       } catch (error){ 
+        console.log("oupsi" + error.message)
+       }
+
+    })
+}
+
+// function showErrorMessage(error){
+
+// }
